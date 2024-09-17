49a1e6063e515782d846c7d9fc8efe9e9f9f824b
JEscobar07 <EscobarjonathanM@outlook.es>
2024-09-17 06:54:38 -0500

    Fix: The reports table is configured by adding the date_report column and the relationship with stateRequest

--- Models/Report.cs
+++ Models/Report.cs
@@ -13,17 +13,27 @@ namespace SkillSwap.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public string Id { get; set; }
+        public int Id { get; set; }
         [Column("title")]
         [MaxLength(100)]
         public required string TitleReport { get; set; }
         [Column("description")]
         [MaxLength(100)]
         public required string Description { get; set; }
+
+        [Column("date_report")]
+        public required DateOnly DateReport { get; set; }
+
+        [Column("id_state_report")]
+        public required int IdState {get;set;}
+
         [Column("id_user")]
         public int IdUser { get; set; }
         [Column("id_user_reported")]
         public int IdReportedUser { get; set; }
+
+        [ForeignKey("IdState")]
+        public StateRequest StateRequest {get; set;} 
 
         [ForeignKey("IdUser")]
         public User User { get; set; }
