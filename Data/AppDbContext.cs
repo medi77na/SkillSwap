a7aa1f999c6e812008902b26f4e0c96c95c4ca10
arlexz96 <arlex.z96@gmail.com>
2024-09-21 19:16:56 -0500

    Feat: add a get of users to sort them by account creation date

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -199,6 +199,7 @@ public partial class AppDbContext : DbContext
             entity.Property(e => e.PhoneNumber)
                 .HasMaxLength(30)
                 .HasColumnName("phone_number");
+            entity.Property(e => e.CreatedAt).HasColumnName("created_at");
             entity.Property(e => e.UrlImage)
                 .HasMaxLength(100)
                 .HasColumnName("url_image");
