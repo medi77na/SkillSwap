8e462b739a7207a9ce08bc240c6b8e14da4fabfd
JEscobar07 <EscobarjonathanM@outlook.es>
2024-09-22 15:42:11 -0500

    Fix:Add Id in the UserPostDTO

--- Dtos/User/UserPostDTO.cs
+++ Dtos/User/UserPostDTO.cs
@@ -1,7 +1,7 @@
 namespace SkillSwap.Dtos.User;
 public class UserPostDTO
 {
-
+    public int Id { get; set; }
     public string? Name { get; set; }
     public string? LastName { get; set; }
     public string? UrlImage { get; set; }
